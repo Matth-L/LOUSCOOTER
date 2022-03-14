44b6284cba565ec495e1f8d85477482c56ee7da7
Matth-L <matthias.lapu@gmail.com>
2022-03-14 08:59:58 +0100

    ajout menu

--- Main.java
+++ Main.java
@@ -1,5 +1,22 @@
-public class Main {
-    public static void main(String[] args) {
+import java.util.Scanner;
 
+public class Main {
+    public static void menu() {
+        String[] option = { "1 : Louer", "2 : Retour ", "3: Etat", "4 : Quitter" };
+        for (int i = 0; i < option.length; i++) {
+            System.out.println(option[i]);
+        }
     }
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int val = 1;
+        while (val != 4) {
+            menu();
+            System.out.print("Entrez un nombre :");
+            val = scan.nextInt();
+        }
+        scan.close();
+    }
+
 }
