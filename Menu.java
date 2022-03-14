32b63f5daca9d3500fac8ebd50396f8aede00308
Matth-L <matthias.lapu@gmail.com>
2022-03-14 09:04:36 +0100

    Séparation Menu

---
+++ Menu.java
@@ -0,0 +1,8 @@
+public class Menu extends Main {
+    public void menu() {
+        String[] option = { "1 : Louer", "2 : Retour ", "3: Etat", "4 : Quitter" };
+        for (int i = 0; i < option.length; i++) {
+            System.out.println(option[i]);
+        }
+    }
+}
