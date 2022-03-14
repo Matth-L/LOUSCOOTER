47eca4447d74c51c9fecb6d461292211de5e90d1
Matth-L <matthias.lapu@gmail.com>
2022-03-14 11:24:39 +0100

    init

--- Main.java
+++ Main.java
@@ -6,6 +6,10 @@ public class Main {
         Menu affiche = new Menu();
         int val = 1;
         while (val != 4) {
+            // nettoie la console
+            System.out.print("\033[H\033[2J");
+            System.out.flush();
+            // menu
             affiche.menu();
             System.out.print("Entrez un nombre : ");
             val = scan.nextInt();
--- Menu.java
+++ Menu.java
@@ -1,6 +1,6 @@
 public class Menu extends Main {
     public void menu() {
-        String[] option = { "1 : Louer", "2 : Retour ", "3: Etat", "4 : Quitter" };
+        String[] option = { "1 : Louer", "2 : Retour ", "3 : Etat", "4 : Quitter" };
         for (int i = 0; i < option.length; i++) {
             System.out.println(option[i]);
         }
