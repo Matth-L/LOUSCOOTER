2b47bfd963f35a8cb3ca2af97c0394dea89182f5
worlcrafte <leo.flandin10@orange.fr>
2022-03-17 11:01:10 +0100

    1

--- Scooter.java
+++ Scooter.java
@@ -1,3 +1,29 @@
 public class Scooter {
+    private int id;
+    private boolean etat;
+    private int kilometrage;
+    private String marque;
+    private String modele;
 
+    public int getId (){
+        return this.id;
+    }
+    public boolean getEtat(){
+        return this.etat;
+    }
+    public int getKilometrage(){
+        return this.kilometrage;
+    }
+    public String getMarque(){
+        return this.marque;
+    }
+    public String getModele(){
+        return this.modele;
+    }
+    public void setId(int x){
+        this.id=x;
+    }
+    public void setEtat (boolean x){
+        this.etat=x;
+    }
 }
