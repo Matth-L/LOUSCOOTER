a652c9f1c3a8affe74c6e0777ff93f805bc66380
Matth-L <matthias.lapu@gmail.com>
2022-03-17 11:00:28 +0100

    ajout ArayList + changement de variable

--- Menu.java
+++ Menu.java
@@ -1,6 +1,8 @@
 public class Menu extends ParcAuto {
     public void menu() {
-        String[] option = { "1 : Louer", "2 : Retour ", "3 : Etat", "4 : Quitter" };
+        String[] option = { "1 : Louer un scooter", "2 : Retour d'un scooter ", "3 : Etat d'un scooter",
+                "4 : Affichage de l'état du parc des scooters ", "5 : Saisie du parc des scooters ",
+                "6 : Quitter le programme " };
         for (int i = 0; i < option.length; i++) {
             System.out.println(option[i]);
         }
--- ParcAuto.java
+++ ParcAuto.java
@@ -1,11 +1,15 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class ParcAuto {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        Menu affiche = new Menu();
+        ArrayList<Scooter> tabScooter = new ArrayList();// L'arrayList qui va servir pour le tableau d'objet
+        // pour ajouter des scooters dans la base de donnée il faut faire
+        // tabScooter.add()
+        Menu affiche = new Menu(); // affiche le menu
         int val = 1;
-        while (val != 4) {
+        while (val != 6) {
             // nettoie la console
             System.out.print("\033[H\033[2J");
             System.out.flush();
