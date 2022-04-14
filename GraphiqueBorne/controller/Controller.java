ae9a66f85441d7bae376027c066f1d9ae0ebd3d0
Matth-L <matthias.lapu@gmail.com>
2022-04-14 10:45:08 +0200

    correction

--- GraphiqueBorne/controller/Controller.java
+++ GraphiqueBorne/controller/Controller.java
@@ -10,38 +10,45 @@ public class Controller {
 
     // @Override
     // public void actionPerformed(ActionEvent e) {
-    //     if ((((JButton) e.getSource()).getText()).equals("Louer")) {
-    //         System.out.println("louer");
-    //     }
-    //     if ((((JButton) e.getSource()).getText()).equals("Retour")) {
-    //     }
-    //     if ((((JButton) e.getSource()).getText()).equals("État d'un scooter")) {
-    //     }
-    //     if ((((JButton) e.getSource()).getText()).equals("Affichage de l'état du parc de scooters")) {
-    //     }
-    //     if ((((JButton) e.getSource()).getText()).equals("Saisie du parc des scooters")) {
-    //     }
-    //     if ((((JButton) e.getSource()).getText()).equals("Quitter le programme")) {
-    //         System.exit(0);
-    //     }
+    // if ((((JButton) e.getSource()).getText()).equals("Louer")) {
+    // System.out.println("louer");
+    // }
+    // if ((((JButton) e.getSource()).getText()).equals("Retour")) {
+    // }
+    // if ((((JButton) e.getSource()).getText()).equals("État d'un scooter")) {
+    // }
+    // if ((((JButton) e.getSource()).getText()).equals("Affichage de l'état du parc
+    // de scooters")) {
+    // }
+    // if ((((JButton) e.getSource()).getText()).equals("Saisie du parc des
+    // scooters")) {
+    // }
+    // if ((((JButton) e.getSource()).getText()).equals("Quitter le programme")) {
+    // System.exit(0);
+    // }
     // }
 
-    public void btnLouer(ActionEvent e){
+    public void btnLouer(ActionEvent e) {
         System.out.println("louer");
     }
-    public void btnRetour(ActionEvent e){
+
+    public void btnRetour(ActionEvent e) {
         System.out.println("louer");
     }
-    public void btnetatScoot(ActionEvent e){
+
+    public void btnetatScoot(ActionEvent e) {
         System.out.println("etatScoot");
     }
-    public void btnafficheAllScoot(ActionEvent e){
+
+    public void btnafficheAllScoot(ActionEvent e) {
         System.out.println("affiche all");
     }
-    public void btnparcScoot(ActionEvent e){
+
+    public void btnparcScoot(ActionEvent e) {
         System.out.println("parcscoot");
     }
-    public void btnquit(ActionEvent e){
-        System.out.println("parcscoot");
+
+    public void btnquit(ActionEvent e) {
+        System.out.println("quit");
     }
 }
--- GraphiqueBorne/view/Gui.java
+++ GraphiqueBorne/view/Gui.java
@@ -17,17 +17,16 @@ public class Gui extends JFrame {
         setSize(x / 2, y / 2);// taille de l'écran de l'utilisateur par 2 (fullscreen est un peu aggressif)
         // init pannel + bouton
         JPanel panel = new JPanel(new GridLayout(6, 1));
+
         Bouton louer = new Bouton("Louer");
         Bouton retour = new Bouton("Retour");
-        
-        
         Bouton etatScoot = new Bouton("État d'un scooter");
         Bouton afficheAllScoot = new Bouton("Affichage de l'état du parc de scooters");
         Bouton parcScoot = new Bouton("Saisie du parc des scooters");
-        
+
         Bouton quit = new Bouton("Quitter le programme");
         // ajout au panel des boutons au panel
-        louer.addActionListener( new ActionListener(){
+        louer.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 ctr.btnLouer(e);
@@ -35,42 +34,41 @@ public class Gui extends JFrame {
 
         });
 
-        retour.addActionListener(new ActionListener(){
+        retour.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 ctr.btnRetour(e);
             }
 
         });
-        etatScoot.addActionListener(new ActionListener(){
+        etatScoot.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 ctr.btnetatScoot(e);
             }
 
         });
-        afficheAllScoot.addActionListener(new ActionListener(){
+        afficheAllScoot.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 ctr.btnafficheAllScoot(e);
             }
 
         });
-        parcScoot.addActionListener(new ActionListener(){
+        parcScoot.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 ctr.btnparcScoot(e);
             }
 
         });
-        quit.addActionListener(new ActionListener(){
+        quit.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 ctr.btnquit(e);
             }
 
         });
-
 
         panel.add(louer);
         panel.add(retour);
@@ -94,120 +92,121 @@ public class Gui extends JFrame {
     }
 }
 
-
 /*
-package t;
-
-import java.awt.BorderLayout;
-import java.awt.Dimension;
-import java.awt.EventQueue;
-
-import javax.swing.JFrame;
-import javax.swing.JPanel;
-import javax.swing.border.EmptyBorder;
-import javax.swing.JButton;
-import javax.swing.JTextField;
-import javax.swing.Box;
-import java.awt.GridLayout;
-import javax.swing.JLabel;
-import javax.swing.JTextArea;
-import javax.swing.JComboBox;
-import java.awt.FlowLayout;
-import java.awt.event.ActionListener;
-import java.awt.event.ActionEvent;
-import java.awt.GridBagLayout;
-import java.awt.GridBagConstraints;
-import java.awt.Insets;
-import javax.swing.JList;
-import javax.swing.JScrollBar;
-import javax.swing.SwingConstants;
-import javax.swing.DefaultComboBoxModel;
-
-public class toot extends JFrame {
-
-	private JPanel contentPane;
-
-	JButton btnRetour = new JButton("Retour");
-
-	JButton btnLouer = new JButton("Louer");
-	
-	JTextArea textArea = new JTextArea("nb de scooter louer :" );
-		
-	JComboBox scootLouer = new JComboBox();
-	
-	JComboBox ScoutDispo = new JComboBox();
-	
-	JScrollBar scrollBar = new JScrollBar();
-	
-
-	aunch the application.
-	
-	public static void main(String[] args) {
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
-					toot frame = new toot();
-					frame.setVisible(true);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		});
-	}
-
-	
-	 * Create the frame.
-	 
-	public toot() {
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 870, 640);
-		contentPane = new JPanel();
-		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		setContentPane(contentPane);
-		int i=3;
-		
-		btnRetour.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-			}
-		});
-		
-		btnLouer.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-			}
-		});
-		contentPane.setLayout(new FlowLayout(FlowLayout.CENTER, 5, 5));
-		//contentPane.add(btnLouer);
-		//contentPane.add(btnRetour);
-		//contentPane.add(textArea);
-		
-		
-		scootLouer.setModel(new DefaultComboBoxModel(new String[] {"toolbar", "tsdfqsdf", "tsqfqsdfqsdf", "tqsdfqdf", "t"}));
-		//contentPane.add(scootLouer);
-		
-		JTextArea textArea_1 = new JTextArea("nb de scooter disponible :"+i);
-		//contentPane.add(textArea_1);
-		
-		
-		ScoutDispo.setModel(new DefaultComboBoxModel(new String[] {"toolbar", "tsdfqsdf", "tsqfqsdfqsdf", "tqsdfqdf", "t"}));
-		ScoutDispo.setSelectedIndex(0);
-		ScoutDispo.setToolTipText("");
-		//contentPane.add(ScoutDispo);
-		contentPane.add(creatRightPanel(), BorderLayout.CENTER);
-		
-		contentPane.add(scrollBar);
-	}
-	
-	private JPanel creatRightPanel() {
-        JPanel panel = new JPanel(new GridLayout(4, 1));
-        panel.add(btnRetour);
-        panel.add(btnLouer);
-        panel.add(textArea);
-        panel.add(scootLouer);
-
-
-        return panel;
-    }
-
-}
-
-*/
+ * package t;
+ * 
+ * import java.awt.BorderLayout;
+ * import java.awt.Dimension;
+ * import java.awt.EventQueue;
+ * 
+ * import javax.swing.JFrame;
+ * import javax.swing.JPanel;
+ * import javax.swing.border.EmptyBorder;
+ * import javax.swing.JButton;
+ * import javax.swing.JTextField;
+ * import javax.swing.Box;
+ * import java.awt.GridLayout;
+ * import javax.swing.JLabel;
+ * import javax.swing.JTextArea;
+ * import javax.swing.JComboBox;
+ * import java.awt.FlowLayout;
+ * import java.awt.event.ActionListener;
+ * import java.awt.event.ActionEvent;
+ * import java.awt.GridBagLayout;
+ * import java.awt.GridBagConstraints;
+ * import java.awt.Insets;
+ * import javax.swing.JList;
+ * import javax.swing.JScrollBar;
+ * import javax.swing.SwingConstants;
+ * import javax.swing.DefaultComboBoxModel;
+ * 
+ * public class toot extends JFrame {
+ * 
+ * private JPanel contentPane;
+ * 
+ * JButton btnRetour = new JButton("Retour");
+ * 
+ * JButton btnLouer = new JButton("Louer");
+ * 
+ * JTextArea textArea = new JTextArea("nb de scooter louer :" );
+ * 
+ * JComboBox scootLouer = new JComboBox();
+ * 
+ * JComboBox ScoutDispo = new JComboBox();
+ * 
+ * JScrollBar scrollBar = new JScrollBar();
+ * 
+ * 
+ * aunch the application.
+ * 
+ * public static void main(String[] args) {
+ * EventQueue.invokeLater(new Runnable() {
+ * public void run() {
+ * try {
+ * toot frame = new toot();
+ * frame.setVisible(true);
+ * } catch (Exception e) {
+ * e.printStackTrace();
+ * }
+ * }
+ * });
+ * }
+ * 
+ * 
+ * Create the frame.
+ * 
+ * public toot() {
+ * setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+ * setBounds(100, 100, 870, 640);
+ * contentPane = new JPanel();
+ * contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+ * setContentPane(contentPane);
+ * int i=3;
+ * 
+ * btnRetour.addActionListener(new ActionListener() {
+ * public void actionPerformed(ActionEvent e) {
+ * }
+ * });
+ * 
+ * btnLouer.addActionListener(new ActionListener() {
+ * public void actionPerformed(ActionEvent e) {
+ * }
+ * });
+ * contentPane.setLayout(new FlowLayout(FlowLayout.CENTER, 5, 5));
+ * //contentPane.add(btnLouer);
+ * //contentPane.add(btnRetour);
+ * //contentPane.add(textArea);
+ * 
+ * 
+ * scootLouer.setModel(new DefaultComboBoxModel(new String[] {"toolbar",
+ * "tsdfqsdf", "tsqfqsdfqsdf", "tqsdfqdf", "t"}));
+ * //contentPane.add(scootLouer);
+ * 
+ * JTextArea textArea_1 = new JTextArea("nb de scooter disponible :"+i);
+ * //contentPane.add(textArea_1);
+ * 
+ * 
+ * ScoutDispo.setModel(new DefaultComboBoxModel(new String[] {"toolbar",
+ * "tsdfqsdf", "tsqfqsdfqsdf", "tqsdfqdf", "t"}));
+ * ScoutDispo.setSelectedIndex(0);
+ * ScoutDispo.setToolTipText("");
+ * //contentPane.add(ScoutDispo);
+ * contentPane.add(creatRightPanel(), BorderLayout.CENTER);
+ * 
+ * contentPane.add(scrollBar);
+ * }
+ * 
+ * private JPanel creatRightPanel() {
+ * JPanel panel = new JPanel(new GridLayout(4, 1));
+ * panel.add(btnRetour);
+ * panel.add(btnLouer);
+ * panel.add(textArea);
+ * panel.add(scootLouer);
+ * 
+ * 
+ * return panel;
+ * }
+ * 
+ * }
+ * 
+ */
